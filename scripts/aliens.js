f1f88efbc5f40b4cd62821c0aa2b71c10ccf946f
JM Cléry <jmclery@gmail.com>
2018-10-15 19:03:09 +0200

    Refactor alien's sprites handling

--- scripts/aliens.js
+++ scripts/aliens.js
@@ -1,22 +1,29 @@
-const aliensPos = [
+const NB_ALIENS_PER_LINE = 11; // Nombre d'aliens par ligne
+const ALIEN_SPACE_X = 35;
+const ALIEN_SPACE_Y = 28;
+const aliensMap = [
     40, 40, 40, 40, 40, 40, 40, 40, 40, 40, 40,
     20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20,
     20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20,
     10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
     10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10
 ];
-
-const aliensSpriteCoords = {
-    40 : [ [6, 3, 16, 16], [6, 25, 16, 16] ],
-    20 : [ [32, 3, 22, 16], [32, 25, 22, 16] ],
-    10 : [ [60, 3, 24, 16], [60, 25, 24, 16] ]
+const alienSprites = {
+    40 : [
+        { x:6 , y:3  , width:16 , height:16 },
+        { x:6 , y:25 , width:16 , height:16 }
+    ],
+    20 : [
+        { x:32 , y:3  , width:22 , height:16 },
+        { x:32 , y:25 , width:22 , height:16 }
+    ],
+    10 : [
+        { x:60 , y:25 , width:24 , height:16 },
+        { x:60 , y:3  , width:24 , height:16 }
+    ]
 };
-
-const NB_ALIENS_PER_LINE = 11; // Nombre d'aliens par ligne
-
 let aliensTimer = 1000; // intervalle de mouvements d'aliens en milli-secondes
 let lastAlienMovement = 0; // timecode du dernier déplacement des aliens
-let alienSpriteTicker = 0; // Indicateur pour alterner les sprites
 let diedAliensIndex = []; // Tableau contenant les indices des aliens qui sont marqués comme morts
 let alienDiedTimer = 0; // Timer pour laisser apparaître momentanément l'animation de mort sur un alien touché
 
@@ -27,25 +34,24 @@ function createAliens() {
     
     const aliens = [];
 
-    for (let i = 0, line = 0; i < aliensPos.length; i++) {
+    for (let i = 0, line = 0; i < aliensMap.length; i++) {
         if (i % NB_ALIENS_PER_LINE === 0) {
             line++;
         }
 
-        if (aliensPos[i] === 0) continue;
+        if (aliensMap[i] === 0) continue;
+
+        let alienWidth = alienSprites[ aliensMap[i] ][ 0 ].width;
+        let alienHeight = alienSprites[ aliensMap[i] ][ 0 ].height;
         
         aliens.push({
-            x : 10 + i % NB_ALIENS_PER_LINE * 35 + (24 - aliensSpriteCoords[aliensPos[i]][0][2] / 2 | 0),
-            y : 100 + line * 28,
-            points : aliensPos[i],
-            direction : 1, // Facteur multiplicateur pour gérer la direction de l'alien (1 : droite, -1 : gauche)
-            sprite : {
-                img : spritesheet,
-                offsetX : aliensSpriteCoords[aliensPos[i]][0][0],
-                offsetY : aliensSpriteCoords[aliensPos[i]][0][1],
-                width : aliensSpriteCoords[aliensPos[i]][0][2],
-                height : aliensSpriteCoords[aliensPos[i]][0][3]
-            }
+            x           : 10 + i % NB_ALIENS_PER_LINE * ALIEN_SPACE_X + (24 - alienWidth / 2 | 0),
+            y           : 100 + line * ALIEN_SPACE_Y,
+            width       : alienWidth,
+            height      : alienHeight,
+            points      : aliensMap[i],
+            spriteIndex : 0,
+            direction   : 1 // Facteur multiplicateur pour gérer la direction de l'alien (1 : droite, -1 : gauche)
         });
     }
 
@@ -56,6 +62,9 @@ function animateAliens() {
 
     // Mouvement des aliens de gauche à droite et vers le bas
     if (Date.now() - lastAlienMovement > aliensTimer) {
+        
+        lastAlienMovement = Date.now(); // Mise à jour de l'instant du dernier mouvement du joueur à "maintenant"!
+
         // Vérification si un des aliens du groupe a atteint le joueur
         // Pour cela, récupération des coordonnées de l'alien le plus bas dans le groupe
         let extremeDownAlien = Math.max(...aliens.map(alien => alien.y));
@@ -68,14 +77,11 @@ function animateAliens() {
         // Vérification si un changement de direction du groupe d'aliens doit être effectué
         // Pour cela, récupération des coordonnées des 2 aliens se trouvants respectivement aux extrêmes gauche et droite du groupe
         let extremeLeftAlien = Math.min(...aliens.map(alien => alien.x));
-        let extremeRightAlien = Math.max(...aliens.map(alien => alien.x)) + 30;
+        let extremeRightAlien = Math.max(...aliens.map(alien => alien.x)) + ALIEN_SPACE_X;
 
         // Test Booléen pour vérifier si le groupe d'aliens est sur le point de percuter un bord de la zone de jeu
         // Dans ce cas, on stocke ce résultat de ce test (true ou false) dans la variable "aliensWallCollision"
         let aliensWallCollision = (extremeLeftAlien - 12 < 0 && aliens[0].direction === -1 || extremeRightAlien + 12 > canvas.width && aliens[0].direction === 1);
-        
-        alienSpriteTicker++;
-        lastAlienMovement = Date.now();
         
         // Parcours du tableau d'aliens pour mise à jour
         for (let i = 0; i < aliens.length; i++) {
@@ -93,10 +99,7 @@ function animateAliens() {
             }
 
             // Mise à jour des coordonnées des sprites (pour gérer l'animation des aliens)
-            aliens[i].sprite.offsetX = aliensSpriteCoords[aliens[i].points][alienSpriteTicker % 2][0];
-            aliens[i].sprite.offsetY = aliensSpriteCoords[aliens[i].points][alienSpriteTicker % 2][1];
-            aliens[i].sprite.width = aliensSpriteCoords[aliens[i].points][alienSpriteTicker % 2][2];
-            aliens[i].sprite.height = aliensSpriteCoords[aliens[i].points][alienSpriteTicker % 2][3];
+            aliens[i].spriteIndex = (aliens[i].spriteIndex + 1) % 2;
 
             // Un alien peut occasionnellement tirer...
             alienShot(aliens[i]);
@@ -113,15 +116,15 @@ function animateAliens() {
             if (diedAliensIndex.includes(i)) continue;
 
             if (player.bullet.x >= aliens[i].x &&
-                player.bullet.x + player.bullet.width <= aliens[i].x + aliens[i].sprite.width &&
-                player.bullet.y <= aliens[i].y + aliens[i].sprite.height &&
+                player.bullet.x + player.bullet.width <= aliens[i].x + aliens[i].width &&
+                player.bullet.y <= aliens[i].y + aliens[i].height &&
                 player.bullet.y + player.bullet.height > aliens[i].y) {
                 // Collision!
-                aliens[i].x = aliens[i].x + aliens[i].sprite.width/2 - (26 / 2) | 0;
+                /* aliens[i].x = aliens[i].x + aliens[i].sprite.width/2 - (26 / 2) | 0;
                 aliens[i].sprite.offsetX = 88;
                 aliens[i].sprite.offsetY = 25;
                 aliens[i].sprite.width = 26;
-                aliens[i].sprite.height = 16;
+                aliens[i].sprite.height = 16; */
                 // Marquage de l'alien comme "tué", ce qui l'affichera avec le sprite de collision, juste avant de disparaître après un timer
                 diedAliensIndex.push(i);
                 alienDiedTimer = Date.now();
@@ -197,18 +200,22 @@ function animateAliens() {
 function renderAliens() {
 
     for (let i = 0; i < aliens.length; i++) {
+        
+        let points      = aliens[i].points;
+        let spriteIndex = aliens[i].spriteIndex;
+
         context.drawImage(
-            aliens[i].sprite.img,
+            spritesheet,
     
-            aliens[i].sprite.offsetX,
-            aliens[i].sprite.offsetY,
-            aliens[i].sprite.width,
-            aliens[i].sprite.height,
+            alienSprites[points][spriteIndex].x,
+            alienSprites[points][spriteIndex].y,
+            alienSprites[points][spriteIndex].width,
+            alienSprites[points][spriteIndex].height,
     
             aliens[i].x,
             aliens[i].y,
-            aliens[i].sprite.width,
-            aliens[i].sprite.height
+            alienSprites[points][spriteIndex].width,
+            alienSprites[points][spriteIndex].height
         );
     }
 
@@ -225,8 +232,8 @@ function alienShot(alien) {
         sounds['shoot'].play();
         // Ajout d'un shoot alien dans le tableau correspondant
         aliensShots.push({
-            x : alien.x + alien.sprite.width/2,
-            y : alien.y + alien.sprite.height,
+            x : alien.x + alienSprites[alien.points][alien.spriteIndex].width/2,
+            y : alien.y + alienSprites[alien.points][alien.spriteIndex].height,
             width : 4,
             height : 10
         });
